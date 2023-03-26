29aa24cf73b2a7c8f66692a936ac0d48df8d2265
nub06 <57972523+nub06@users.noreply.github.com>
2023-03-26 17:14:26 +0300

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -13,18 +13,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-jsoncompare",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Compares two files and displays the differences",
+	Long: `It is used to compare two files and display the differences between them. The command takes two file paths as input arguments and usage of using input:
+	e.g. "go-jsoncompare file1 file2"`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
 
 		compareFiles(args)
@@ -58,13 +52,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-jsoncompare.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
